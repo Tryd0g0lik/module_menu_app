90657ee81875ebdd642b7fe9a7e6023f6a475e69
Tryd0g0lik <work80@mail.ru>
2025-04-25 11:19:55 +0700

    [master]: the first migrate was added

--- menu_app/models.py
+++ menu_app/models.py
@@ -2,11 +2,29 @@
 menu_app/models.py
 """
 
-# from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 
 
-class UserRegister(AbstractUser):
-    pass
+class CustomUser(AbstractUser):
+    groups = models.ManyToManyField(
+        "auth.Group",
+        related_name="%(class)s_groups",
+        blank=True,
+        help_text=_(
+            "The groups this user belongs to. A user will get all permissions "
+            "granted to each of their groups."
+        ),
+        verbose_name="groups",
+    )
+    user_permissions = models.ManyToManyField(
+        "auth.Permission",
+        related_name="%(class)s_user_permissions",
+        # Уникальное имя для обратной связи
+        blank=True,
+        help_text=_("Specific permissions for this user."),
+        verbose_name="user permissions",
+    )
