ba38e015ef62b50aee555ab1d1e94cd08846861e
Tryd0g0lik <work80@mail.ru>
2025-05-13 16:51:29 +0700

    [masters]: update

--- pull.sh
+++ pull.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd /home/denis/module_menu_app  || exit 1
 #git pull --ff-only https://github.com/Tryd0g0lik/module_menu_app.git tests  || exit 1
-git pull https://github.com/Tryd0g0lik/module_menu_app.git tests  || exit 1
-#git fetch https://github.com/Tryd0g0lik/module_menu_app.git tests
+# git pull https://github.com/Tryd0g0lik/module_menu_app.git tests  || exit 1
+git fetch https://github.com/Tryd0g0lik/module_menu_app.git tests
 #git reset --hard https://github.com/Tryd0g0lik/module_menu_app.git origin/test
 git add * || exit 1
 git commit -m "Update" || exit 1
