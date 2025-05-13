04447e2aa1950662da971e009b47562b0e35e343
Tryd0g0lik <work80@mail.ru>
2025-05-13 18:06:51 +0700

    [masters]: update

--- pull.sh
+++ pull.sh
@@ -4,6 +4,11 @@ cd /home/denis/module_menu_app  || exit 1
 # git pull https://github.com/Tryd0g0lik/module_menu_app.git tests  || exit 1
 git fetch origin tests
 # git reset --hard origin/test
+git checkout remotes/origin/tests
+git add *
+git checkout tests
+git add *
+git commit -m "Update" || exit 1
 git merge --no--ff remotes/origin/tests
 git add * || exit 1
 git commit -m "Update" || exit 1
