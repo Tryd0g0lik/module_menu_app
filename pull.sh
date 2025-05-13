c8d46a4bae1eb41fa10b1abde2dd9d921ae3ddc3
Tryd0g0lik <work80@mail.ru>
2025-05-13 17:31:06 +0700

    [masters]: update

--- pull.sh
+++ pull.sh
@@ -4,7 +4,7 @@ cd /home/denis/module_menu_app  || exit 1
 # git pull https://github.com/Tryd0g0lik/module_menu_app.git tests  || exit 1
 git fetch origin tests
 # git reset --hard origin/test
-git merge --cached remotes/origin/tests
+git merge --no--ff remotes/origin/tests
 git add * || exit 1
 git commit -m "Update" || exit 1
 sudo systemctl restart gunicorn  || exit 1
